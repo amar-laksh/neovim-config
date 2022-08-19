69639fc643e1f2401b179b247ae843ccc9129c6c
Amar Lakshya <amar.lakshya@protonmail.com>
2022-08-19 19:28:10 +0100

    cleaned up packer_init

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -55,16 +55,17 @@ return packer.startup(function(use)
     -------------------------------
     -- Experience Plugins
     -------------------------------
-    use "ethanholz/nvim-lastplace" -- restore last position of the cursor
     use "scrooloose/nerdcommenter" --Comment god
+    use "chentau/marks.nvim" -- better marks support
+    use "machakann/vim-sandwich" -- perform operations between pairs of symbols
+    use "terryma/vim-multiple-cursors" -- yep
+
+    use "ethanholz/nvim-lastplace" -- restore last position of the cursor
     use {
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     }
-    use "chentau/marks.nvim" -- better marks support
 
-    use "machakann/vim-sandwich" -- perform operations between pairs of symbols
-    use "terryma/vim-multiple-cursors" -- yep
 
     use "neovim/nvim-lspconfig" -- Configurations for Nvim LSP
     use "hrsh7th/nvim-cmp" -- Autocompletion plugin
@@ -81,18 +82,6 @@ return packer.startup(function(use)
             }
         end
     }
-    use "mizlan/iswap.nvim"
-    use { "michaelb/sniprun", run = "bash ./install.sh" }
-    use "p00f/clangd_extensions.nvim"
-
-    use {
-        "nvim-telescope/telescope.nvim", tag = "0.1.0",
-        -- or                            , branch = "0.1.x",
-        requires = { { "nvim-lua/plenary.nvim" } }
-    }
-    use "s1n7ax/nvim-terminal"
-
-
     use({
         "iamcco/markdown-preview.nvim",
         run = function() vim.fn["mkdp#util#install"]() end,
@@ -103,12 +92,22 @@ return packer.startup(function(use)
             require("spellsitter").setup()
         end
     }
-    use "stevearc/aerial.nvim"
     use { "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim",
         config = function()
             require("neogit").setup()
         end
     }
+    use {
+        "nvim-telescope/telescope.nvim", tag = "0.1.0",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
+
+    use "mizlan/iswap.nvim"
+    use { "michaelb/sniprun", run = "bash ./install.sh" }
+    use "p00f/clangd_extensions.nvim"
+    use "s1n7ax/nvim-terminal"
+    use "stevearc/aerial.nvim"
+
     use "stevearc/dressing.nvim"
     use "mrjones2014/legendary.nvim"
 
