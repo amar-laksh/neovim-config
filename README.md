869f2da6667b6ac0cc53210513ae85d858283386
Amar Lakshya <amar.lakshya@protonmail.com>
2022-10-10 18:30:08 +0100

    fixes: bash install script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # neovim-conf
 My personal neovim config consisting of only lua based plugins 
+
+#  Installation
+
+```bash
+curl https://raw.githubusercontent.com/amar-laksh/neovim-config/main/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,23 +1,22 @@
-#!/bin/bash -
-
+#!/bin/bash
 set -o nounset                                  # Treat unset variables as an error
 
 function install_nvim() {
     cd /tmp/ && \
-    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && \
-    chmod u+x nvim.appimage && \
-    ./nvim.appimage --appimage-extract && \
-    mv squashfs-root /usr/local/nvim_app && \
-    ln -s /usr/local/nvim_app/AppRun /usr/bin/nvim && \
-    chmod u+x /usr/bin/nvim
+        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && \
+        chmod u+x nvim.appimage && \
+        ./nvim.appimage --appimage-extract && \
+        mv squashfs-root /usr/local/nvim_app && \
+        ln -s /usr/local/nvim_app/AppRun /usr/bin/nvim && \
+        chmod u+x /usr/bin/nvim
 }
 
 function install_treesitter() {
 	cd /tmp && \
-		curl -L https://github.com/tree-sitter/tree-sitter/releases/download/v0.20.7/tree-sitter-linux-x64.gz -o treesitter.gz && \
+        curl -L https://github.com/tree-sitter/tree-sitter/releases/download/v0.20.7/tree-sitter-linux-x64.gz -o treesitter.gz && \
 		gunzip -k treesitter.gz && \
-        	mv treesitter /usr/bin/tree-sitter  && \
-        	chmod u+x /usr/bin/tree-sitter
+        mv treesitter /usr/bin/tree-sitter  && \
+        chmod u+x /usr/bin/tree-sitter
 }
 
 function install_python3() {
@@ -30,4 +29,6 @@ function install_python3() {
 [ -e /usr/bin/tree-sitter ] || install_treesitter
 [ -e /usr/bin/python3 ] || install_python3
 
-rm -rf ~/.config/nvim && git clone https://github.com/amar-laksh/neovim-config ~/.config/nvim && cd ~/.config/nvim && git remote set-url origin git@github.com:amar-laksh/neovim-config.git && nvim +PackerSync
+rm -rf ~/.config/nvim && \
+    git clone https://github.com/amar-laksh/neovim-config ~/.config/nvim && cd ~/.config/nvim && \
+    git remote set-url origin git@github.com:amar-laksh/neovim-config.git && nvim +PackerSync
